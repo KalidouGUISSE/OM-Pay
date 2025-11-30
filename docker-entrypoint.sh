7df3c3bed4e5b9b1bec8810d671b3771cd7a1994
Kalidou GUISSE <kalidouguisse16@gmail.com>
2025-11-30 10:07:51 +0000

    y

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -41,6 +41,9 @@ php artisan route:clear || true
 php artisan config:clear || true
 php artisan cache:clear || true
 
+php artisan passport:install
+
+
 # Run migrations (non-blocking failure allowed)
 php artisan migrate --force || true
 
